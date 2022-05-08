546c1eee846ea6050c44aa0d67e7c1e4f1cfbe59
soikatkhan61 <soikatkhan61@gmail.com>
2022-05-08 17:57:51 +0600

    develope buble sort

---
+++ sorting.sh
@@ -0,0 +1,26 @@
+function bubble(){
+#buuble sort
+for ((i=0; i<6; i++))
+do
+    for ((j=0;j<6-i-1;j++))
+    do
+        if [ ${arr[j]} -gt ${arr[j+1]} ]
+        then
+            #swap
+            temp=${arr[j]}
+            arr[$j]=${arr[$((j+1))]}
+            arr[$((j+1))]=$temp
+        fi
+    done
+done
+}
+
+arr=( 10 8 20 100 12 2 )
+
+echo "Original Order of this array"
+echo ${arr[*]}
+
+bubble 
+
+echo "Sorted array is:"
+echo ${arr[*]}
