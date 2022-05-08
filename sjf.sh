c8a5e2cf69b56d927b6ee359053b83c80d1d1445
soikatkhan61 <soikatkhan61@gmail.com>
2022-05-09 04:09:11 +0600

    fixing a bug

--- sjf.sh
+++ sjf.sh
@@ -8,8 +8,7 @@ tat=()
 echo "Enter the number of process"
 read n
 
-for (( i=0;i<n;i++ ))
-do
+for ((i = 0; i < n; i++)); do
     p[i]=$i
     echo "Enter Burst time for process P$i"
     read input
@@ -18,46 +17,45 @@ done
 
 echo ${bt[*]}
 
-for ((i=0;i<n;i++))
-do
-    for (( k=$(($i + 1)) ; k<n ;k++))
-    do
-        temp=${bt[i]}
-        bt[i]=${bt[k]}
-        bt[k]=$temp
+for ((i = 0; i < n; i++)); do
+    for ((k = $(($i + 1)); k < n; k++)); do
+        if [ ${bt[i]} -gt ${bt[k]} ]; then
 
-        temp=${p[i]}
-        p[i]=${p[k]}
-        p[k]=$temp
+            temp=${bt[i]}
+            bt[i]=${bt[k]}
+            bt[k]=$temp
+
+            temp=${p[i]}
+            p[i]=${p[k]}
+            p[k]=$temp
+        fi
     done
 
 done
 
 wt[0]=0
-wtavf=0
+wtavg=0
 
 tat[0]=${bt[0]}
 tatavg=${bt[0]}
 
-for ((i=1;i<n;i++))
-do
-    wt[i]=${tat[$(($i-1))]}
-    tat[i]=$((${wt[i]}+${bt[i]}))
-    wtavg=$(($wtavg+${wt[i]}))
-    tatavg=$(($tatavg+${tat[i]}))
+for ((i = 1; i < n; i++)); do
+    wt[i]=${tat[$(($i - 1))]}
+    tat[i]=$((${wt[i]} + ${bt[i]}))
+    wtavg=$(($wtavg + ${wt[i]}))
+    tatavg=$(($tatavg + ${tat[i]}))
 done
 
 echo -e "\n\t Process \tBurst Time \t Waiting Time \tTurnaround Time\n"
 
-for ((i=0;i<n;i++))
-do
+for ((i = 0; i < n; i++)); do
     echo -e "\n\t P${p[i]} \t\t ${bt[i]} \t\t ${wt[i]} \t\t ${tat[i]} "
 done
 
 echo -e "\n\n"
-echo "Average Waiting time --> $(($wtavg/$n))"
+echo "Average Waiting time --> $(($wtavg / $n))"
 echo -e "\n"
 
 echo -e "\n\n"
-echo "Average Turnaround time --> $(($tatavg/$n))"
+echo "Average Turnaround time --> $(($tatavg / $n))"
 echo -e "\n"
